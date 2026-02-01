76968ec1beffd201697cb18cada516f674ab4cf0
Nancy Bankson <nbankson3@gmail.com>
2026-02-01 09:43:02 -0600

    Updated user model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require("./config/connection");
 require("dotenv").config();
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const userRouter = require("./routes/userRoutes");
 const recipeRouter = require("./routes/recipeRoutes");
@@ -11,6 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 
 app.use('/api/users', userRouter);
 app.use('/api/recipes', recipeRouter);
